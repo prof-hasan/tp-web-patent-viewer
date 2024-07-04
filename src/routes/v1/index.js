6ee0d89002b5284096d8ea093107ff71a505747c
Lucas Rassilan Vilanova <lucasrsv1@gmail.com>
2024-07-04 00:29:58 -0300

    Rotas de estatísticas

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,9 @@
 const v1 = require("express").Router();
 
 const generalRoutes = require("./general");
+const statisticsRoutes = require("./statistics");
 
 v1.use("/", generalRoutes);
+v1.use("/statistics", statisticsRoutes);
 
 module.exports = v1;
--- src/utils/http-validation.js
+++ src/utils/http-validation.js
@@ -4,15 +4,14 @@ const { validationResult } = require("express-validator");
  * Realiza a validação da requisição com base nas regras do express-validator.
  * @param {import("express").Request} req
  * @param {import("express").Response} res
- * @returns {boolean} Informa se a requisição é inválida.
+ * @param {import("express").NextFunction} next
  */
-function isRequestInvalid (req, res) {
-	const validation = validationResult(req);
-	if (validation.isEmpty())
-		return false;
+function validateRequest (req, res, next) {
+	const errors = validationResult(req);
+	if (errors.isEmpty())
+		return next();
 
-	res.status(400).json({ message: validation.array()[0].msg, errors: validation.array() });
-	return true;
+	res.status(400).json({ message: errors.array()[0].msg, errors: errors.array() });
 }
 
-module.exports = { isRequestInvalid };
+module.exports = { validateRequest };
