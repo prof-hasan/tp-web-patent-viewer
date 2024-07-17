b2e5649605d6b4169c8616e6e61cc0cd855027bb
Lucas Rassilan Vilanova <lucasrsv1@gmail.com>
2024-07-17 14:21:19 -0300

    Trata mais alguns despachos de patentes

--- src/services/status-patente.js
+++ src/services/status-patente.js
@@ -5,6 +5,7 @@ const despachosArquivamento = [
 	"4.3.1", "8.6", "11.1", "11.5", "11.12", "11.17",
 	"8.11", "11.1.1", "11.4", "11.2", "11.6", "11.11",
 	"9.2", "9.2.3", "9.2.4",
+	"15.21",
 
 	// Programas de Computador
 	"106", "709", "742",
@@ -16,6 +17,7 @@ const despachosArquivamento = [
 const despachosConcessao = [
 	// Patentes
 	"16.1", "16.3",
+	"9.1",
 
 	// Programas de Computador
 	"120", "730", "745",
@@ -27,7 +29,9 @@ const despachosConcessao = [
 const despachosRetomarAndamento = [
 	// Patentes
 	"11.14", "8.8", "16.2", "16.4",
+	"9.1.1", "9.1.2",
 	"9.2.1", "9.2.2", "9.2.4.1",
+	"15.30",
 
 	// Programas de Computador
 	"743", "744"
