cbf2d7822683183c9c890c91426d39632a7f8f34
Lucas Rassilan Vilanova <lucasrsv1@gmail.com>
2024-08-04 01:29:00 -0300

    Back button

--- src/front-end/src/app/components/header/header.component.ts
+++ src/front-end/src/app/components/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
 import { MatIcon } from "@angular/material/icon";
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Location, NgIf } from "@angular/common";
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from "@angular/router";
 
 import { CollapseModule } from "ngx-bootstrap/collapse";
+
+import { filter } from "rxjs";
 
 @Component({
 	selector: "app-header",
@@ -10,6 +13,7 @@ import { CollapseModule } from "ngx-bootstrap/collapse";
 	imports: [
 		CollapseModule,
 		MatIcon,
+		NgIf,
 		RouterLink,
 		RouterLinkActive
 	],
@@ -17,5 +21,19 @@ import { CollapseModule } from "ngx-bootstrap/collapse";
 	styleUrl: "./header.component.scss"
 })
 export class HeaderComponent {
+	public hideBackButton: boolean = true;
 	public isMenuCollapsed: boolean = true;
+
+	constructor (
+		private readonly location: Location,
+		private readonly router: Router
+	) {
+		this.router.events
+			.pipe(filter(event => event instanceof NavigationEnd))
+			.subscribe(() => this.hideBackButton = this.location.isCurrentPathEqualTo("/statistics"));
+	}
+
+	public goBack (): void {
+		this.location.back();
+	}
 }
