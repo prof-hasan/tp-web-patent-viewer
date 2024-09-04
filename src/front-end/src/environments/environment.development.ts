5480504db5fef1b343fd24a8938dc845eb9f5143
Lucas Rassilan Vilanova <lucasrsv1@gmail.com>
2024-09-03 23:56:17 -0300

    Version bump front-end (v1.1.0)

--- src/front-end/src/environments/environment.development.ts
+++ src/front-end/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: false,
-	version: "1.0.0-dev",
+	version: "1.1.0-dev",
 	API_URL: "http://localhost:3000/api"
 };
--- src/front-end/src/environments/environment.ts
+++ src/front-end/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	version: "1.0.0",
+	version: "1.1.0",
 	API_URL: "/api"
 };
